e4321b193085c99e4d319e1bbd16a9c25aa3505f
IbrahimFBAK <ibrahimfaruk.bin.abulkashem@g.bracu.ac.bd>
2026-06-10 14:32:13 +0600

    Delete src directory

--- src/hooks/useToast.js
+++
@@ -1,9 +0,0 @@
-import { useState, useCallback } from 'react'
-export function useToast() {
-  const [toast, setToast] = useState(null)
-  const showToast = useCallback((msg, ms=2600) => {
-    setToast(msg)
-    setTimeout(() => setToast(null), ms)
-  }, [])
-  return { toast, showToast }
-}
